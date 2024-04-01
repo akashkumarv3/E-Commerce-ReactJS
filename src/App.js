63598b709c3534fb2fe03657f77349f4e82c5eba
akashkumarv3 <87967921+akashkumarv3@users.noreply.github.com>
2024-04-01 11:39:23 +0530

    Created pages for Cart ,login,product,shop and shopCategory.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Cart from './Pages/Cart';
+import LoginSignUp from './Pages/LoginSignUp';
+import Product from './Pages/Product';
+import ShopCategory from './Pages/ShopCategory';
 
 function App() {
   return (
     <div >
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/login' element={<LoginSignUp/>}/>
+        <Route path='/product' element={<Product/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    const[Menu,setMenu]=useState("shop")
+   const[Menu,setMenu]=useState("shop")
+    // Initialize state with the value from local storage if it exists,
+  // otherwise use a default value
+ // const [Menu,setMenu] = useState(
+ //   localStorage.getItem('Menu') || 'shop'
+ // );
+
+  // Update local storage whenever the state changes
+ // useEffect(() => {
+  //  localStorage.setItem('Menu', Menu);
+ // }, [Menu]);
+
   return (
     <div className="navbar">
         <div className="nav-logo">
@@ -12,14 +24,14 @@ function Navbar() {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}>Shop {Menu==="shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}>Men{Menu==="mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}>Women{Menu==="womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids{Menu==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{textDecoration:'none'}}>Shop</Link>{Menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><Link to='/mens' style={{textDecoration:'none'}}>Men</Link>{Menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link to='/womens' style={{textDecoration:'none'}}>Women</Link>{Menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link to='/kids' style={{textDecoration:'none'}}>Kids</Link>{Menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="" />
+        <Link to='/login'><button>Login</button></Link> 
+          <Link to='/cart'><img src={cart_icon} alt="" /></Link>  
             <div className="nav-bar-count">0</div>
         </div>
     </div>
