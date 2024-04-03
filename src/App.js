34598d4a8765df17adac4508d17983559bd84cc4
akashkumarv3 <87967921+akashkumarv3@users.noreply.github.com>
2024-04-03 19:44:29 +0530

    Shop category added for all category and routed.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import LoginSignUp from './Pages/LoginSignUp';
 import Product from './Pages/Product';
 import ShopCategory from './Pages/ShopCategory';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+
+
 
 function App() {
   return (
@@ -17,9 +22,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Shop/>}/>
         <Route path='/cart' element={<Cart/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory  banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path='/login' element={<LoginSignUp/>}/>
         <Route path='/product' element={<Product/>}/>
       </Routes>
